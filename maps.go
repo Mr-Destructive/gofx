a792046091cee9b3685806de77045bcd0e27b945
mr_destructive <gormeet711@gmail.com>
2023-11-21 22:15:57 +0530

    aded dict to return key-value pairs of any object

--- maps.go
+++ maps.go
@@ -51,3 +51,34 @@ func (m *Map[K, V]) Values() []V {
 func (m *Map[K, V]) Len() int {
 	return len(*m)
 }
+
+func Dict(obj interface{}) map[string]interface{} {
+	result := make(map[string]interface{})
+
+	val := reflect.ValueOf(obj)
+
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	switch val.Kind() {
+	case reflect.Struct:
+		typ := val.Type()
+		for i := 0; i < val.NumField(); i++ {
+			field := val.Field(i)
+			fieldName := typ.Field(i).Name
+			result[fieldName] = field.Interface()
+		}
+	case reflect.Map:
+		iter := val.MapRange()
+		for iter.Next() {
+			key := iter.Key()
+			value := iter.Value()
+			result[fmt.Sprintf("%v", key.Interface())] = value.Interface()
+		}
+	default:
+		result["value"] = val.Interface()
+	}
+
+	return result
+}
